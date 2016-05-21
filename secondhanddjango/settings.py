e779e55ec62f2faf182c987ad39c24b3d9924ff5
yuechen <yuechen@yuechen-macbookpro.roam.corp.google.com>
2016-05-21 14:37:23 -0700

    django config for heroku

--- secondhanddjango/settings.py
+++ secondhanddjango/settings.py
@@ -102,9 +102,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ".", "static")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)

4adaeda2ea2a0dada4766ff7a5ed3a7e6afd68cf
yuechen <yuechen@yuechen-macbookpro.roam.corp.google.com>
2016-05-21 13:42:46 -0700

    scrapy request

--- listing/views.py
+++ listing/views.py
@@ -1,6 +1,12 @@
+import base64
+import requests
+
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 from listing.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
@@ -12,10 +18,23 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class ListItems(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        private_key = "8a94547f3ac24f18ab3dfac27602edb4"
+        b64 = base64.encodestring('%s:' % private_key).replace('\n', '')
+        auth = "Basic %s" % b64
+        headers = {'Content-type': 'application/json', 'Authorization': auth}
+        url = 'https://storage.scrapinghub.com/items/65427/1/2'
+        return Response(requests.get(url, headers=headers).text)
--- secondhanddjango/urls.py
+++ secondhanddjango/urls.py
@@ -25,5 +25,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^items[/]?$', views.ListItems.as_view(), name='list_items'),
 ]

88bedceeea20b27f3024ec798e68e2fc9e65e82f
Kevin <yuechen1989@gmail.com>
2016-06-23 04:51:23 +0000

    index and css

--- listing/urls.py
+++ listing/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from listing import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^fetchMoonbbs$', views.fetchMoonbbs, name='fetchMoonbbs'),
     url(r'^fetchChineseInSFBay$', views.fetchChineseInSFBay, name='fetchChineseInSFBay'),
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,7 +1,8 @@
 import base64
 import requests
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
 from django.views.generic import View
@@ -14,6 +15,9 @@ from listing.serializers import UserSerializer, GroupSerializer, PostSerializer
 from listing.models import Post
 
 # Create your views here.
+
+def index(request):
+    return render_to_response('listing/index.html', {}, context_instance=RequestContext(request))
 
 class UserViewSet(viewsets.ModelViewSet):
     """

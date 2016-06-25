a8f032a318d5e9695607f7c6c089a7fbc8e789b1
Liso <yuechen1989@gmail.com>
2016-06-24 22:09:15 -0700

    fix time

--- listing/models.py
+++ listing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 TAG_CHOICES = (
     ('0', 'Sale'),
@@ -12,3 +13,9 @@ class Post(models.Model):
     tag = models.CharField(max_length=1, choices=TAG_CHOICES)
     last_updated_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_past_due(self):
+        if datetime.today() - timedelta(days=1) > self.last_updated_at.replace(tzinfo=None):
+            return True
+        return False

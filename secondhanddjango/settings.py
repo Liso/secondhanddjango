b3277b48f8e94efab477cfb31cac41aa6999c196
Liso <yuechen1989@gmail.com>
2016-07-13 21:13:06 -0700

    cleardb for prod

--- secondhanddjango/settings.py
+++ secondhanddjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
+import urlparse
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,13 +91,40 @@ WSGI_APPLICATION = 'secondhanddjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+  DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+  }
+else:
+  # Register database schemes in URLs.
+  urlparse.uses_netloc.append('mysql')
 
+  try:
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+  except Exception:
+    print 'Unexpected error:', sys.exc_info()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/

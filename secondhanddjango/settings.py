7daf978e939eda32945a825a38b6b6f1f603bbc9
Liso <yuechen1989@gmail.com>
2016-07-25 21:19:41 -0700

    default login url

--- secondhanddjango/settings.py
+++ secondhanddjango/settings.py
@@ -188,6 +188,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+LOGIN_URL = 'admin:login'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
